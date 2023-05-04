d862a160593d1644a647d2fac0f5ced07c8301fc
Giovane Henrique <giovanehenrique7@hotmail.com>
2023-05-04 17:39:25 -0300

    Dados Cadastrais

---
+++ Java-TestUnit/src/main/ProgramaCadastro.java
@@ -0,0 +1,54 @@
+package com.Giovane.QA;
+import java.util.Scanner;
+
+
+public class ProgramaCadastro {
+	
+	public static void main (String[]Args) {
+		Scanner input = new Scanner (System.in);
+		String cpf="", nome = "";
+		int idade =0;
+		
+		
+		try {
+			System.out.println("Olá!! Digite seu nome:");
+			nome = input.nextLine();
+			nome = nome.trim().toUpperCase();
+			Validacao.validarNome(nome);
+			System.out.println("Olá, " + nome + "!");
+		}
+		catch (RuntimeException e) {
+			System.out.println(e.getMessage());
+			System.exit (0);
+		}
+		
+		try {
+			System.out.println("Agora digite seu CPF:");
+			cpf = input.nextLine();
+			Validacao.validarCpf(cpf);
+			System.out.println("Seja bem-vindo(a) "+nome+"!");
+		}
+		catch (RuntimeException e) {
+			System.out.println(e.getMessage());
+			System.exit(0);
+		
+		}
+		
+		try {
+			System.out.println("Agora digite sua idade:");
+			idade = input.nextInt();
+			Validacao.validarIdade(idade);
+		}
+		catch (RuntimeException e) {
+			System.out.println(e.getMessage());
+			System.exit(0);
+		}
+		
+		Validacao valido = new Validacao (nome,cpf,idade);
+		System.out.println(valido.toString());
+		
+		input.close ();
+}
+}	
+	
+	
---
+++ Java-TestUnit/src/main/Validacao.java
@@ -0,0 +1,76 @@
+package com.Giovane.QA;
+
+	public class Validacao {	
+		
+		private String nome;
+		private String cpf; 
+		private int idade;
+		
+		public Validacao () {}
+		
+		public Validacao(String nome,String cpf,int idade) {
+			this.nome = nome;
+			this.cpf = Validacao.formatarCPF(cpf);
+			this.idade = idade;
+		}
+	
+		public static void validarNome (String nome) {
+		 
+			if (nome.trim().isEmpty()) 
+	         throw new RuntimeException("O nome não pode ser vazio.");
+		}
+			
+				
+		public static void validarCpf (String cpf) {
+		
+			if (cpf.trim().length()!= 11) 	
+			 throw new RuntimeException("CPF inválido, tente novamente.");			
+		}
+		
+		public static void validarIdade (int idade) {
+			
+			if (idade<0||idade>120) 
+				throw new RuntimeException("Idade inválida!");
+		}
+	   
+		public static String formatarCPF(String cpf) {
+	       cpf = cpf.replaceAll("[^\\d]", ""); // remove tudo que não for dígito
+	       cpf = cpf.replaceFirst("(\\d{3})(\\d{3})(\\d{3})(\\d{2})", "$1.$2.$3-$4");
+	       return cpf;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			Validacao.validarNome(nome);
+			this.nome = nome;
+		}
+
+		public String getCpf() {
+			this.cpf=Validacao.formatarCPF(cpf);
+			return cpf;
+		}
+
+		public void setCpf(String cpf) {
+			Validacao.validarCpf(cpf);
+			this.cpf = cpf;
+		}
+
+		public int getIdade() {
+			return idade;
+		}
+
+		public void setIdade(int idade) {
+			Validacao.validarIdade(idade);
+			this.idade = idade;
+		}
+
+		@Override
+		public String toString() {
+			return "Dados Cadastrais:  Nome:" + nome + "\t CPF:" + cpf + "\t Idade:" + idade + "";
+		}
+		
+		
+}
